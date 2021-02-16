8187db4214233921275258a57a805b0eb6192167
martingratt <martin_gratt@hotmail.com>
2021-02-16 17:00:52 +0100

    views with ejs

--- controllers/jersey_controller.js
+++ controllers/jersey_controller.js
@@ -3,13 +3,22 @@ import {JerseyMysqlStorage} from '../models/jersey_mysql_storage.js'
 export let jerseyController = {
     listJerseys(req, res) {
         JerseyMysqlStorage.getJerseys().then(
-            result => {res.status(200).send(result)}
+            result => {
+                res.render('jerseys', {jerseyArray: result})
+            }
         ).catch(error => {res.status(500).send(error)})
     },
 
     listJersey(id, res) {
         JerseyMysqlStorage.getJersey(id).then(
-            result => {res.status(200).send(result)}
+            result => {
+                // check if object contains values
+                if (Object.keys(result).length) {
+                    res.render('jersey', {jersey: result[0]})
+                } else {
+                     res.render('404')
+                }
+            }
         ).catch(error => {res.status(500).send(error)})
     }
 
